480479740f65e382b82b13a27831d3e03499c739
Kaung Phyo Wai <kaungphyowaidecember@gmail.com>
2020-11-26 13:10:55 +0630

    Development Screen Complete

--- lib/screens/Development.dart
+++ lib/screens/Development.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Development extends StatelessWidget {
@@ -13,90 +14,88 @@ class Development extends StatelessWidget {
               height: 450,
               width: double.infinity,
               fit: BoxFit.cover,
-              image: AssetImage('assets/analystic.png'),
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            Text(
-              "Development",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-                fontFamily: "Montserratboldextra",
-                color: Color.fromARGB(255, 240, 39, 93),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: 40,
-              ),
-              child: Text(
-                "Vasdf sadfwe wes woevnsl asldfwefasdf fsadfsss sfg",
-                textAlign: TextAlign.center,
-                style: TextStyle(),
-              ),
+              image: AssetImage('assets/development.png'),
             ),
             Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 40,
-                    vertical: 30,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 50,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(
-                              Icons.circle,
-                              size: 10,
-                              color: Theme.of(context)
-                                  .primaryColor
-                                  .withOpacity(0.5),
-                            ),
-                            Icon(
-                              Icons.circle,
-                              size: 10,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                            Icon(
-                              Icons.circle,
-                              size: 10,
-                              color: Theme.of(context)
-                                  .primaryColor
-                                  .withOpacity(0.5),
-                            ),
-                          ],
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 35,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Development",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            fontFamily: "Montserratboldextra",
+                            color: Color.fromARGB(255, 240, 39, 93),
+                          ),
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            // Analystic();
-                          }));
-                        },
-                        child: Icon(
-                          Icons.arrow_forward,
-                          size: 40,
-                          color: Theme.of(context).primaryColor,
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
-                    ],
-                  ),
+                        Text(
+                          "Vasdf sadfwe wes woevnsl asldfwefasdf fsadfsss sfg",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              end: Alignment(-0.1, 1.0),
+                              begin: Alignment(0.1, -1.0),
+                              stops: [
+                                0.3,
+                                1.0,
+                              ],
+                              colors: [
+                                Color(0xffFCD35F),
+                                Color(0xffF4B028),
+                              ],
+                            ),
+                            // color: Color(0xffF4B028),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 50,
+                          ),
+                          child: Text(
+                            "Get Starter",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontFamily: 'Montserratbold'),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Text(
+                          'Terms And Condition',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontFamily: 'Montserrat',
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
